05cd8d4f8b406bcecb18b317580e255fc3311eba
Sujit <sujitkhopade2001@gmail.com>
2020-10-14 11:48:30 -0700

    Updates

---
+++ precache-manifest.663af5ef92544f220fbc6f49a2b01194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190782b2aaeb84eb4cdd9a11b7ef68a3",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "4e409828a22ddc861fcf",
+    "url": "/Quiz-App/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "71c802f8d6d8abf71a86",
+    "url": "/Quiz-App/static/js/2.9a66b2a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz-App/static/js/2.9a66b2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e409828a22ddc861fcf",
+    "url": "/Quiz-App/static/js/main.c7745f94.chunk.js"
+  },
+  {
+    "revision": "ed474aac6aa2c5af41fa",
+    "url": "/Quiz-App/static/js/runtime-main.fa0db27d.js"
+  }
+]);
